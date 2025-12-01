ca19ab218d79047363f3a13ed0ac23435b3c3db9
Fazliddin Baxtiyorov <arolboyevfazliddin956@gmail.com>
2025-12-01 15:05:49 +0500

    Adding models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Fanlar(models.Model):
+    birinchi_fan = models.CharField(max_length=120)
+    ikkinchi_fan = models.CharField(max_length=120)
+    uchinchi_fan = models.CharField(max_length=120)
+    tortinchi_fan = models.CharField(max_length=120)
+    beshinchichi_fan = models.CharField(max_length=120)
+    oltinchi_fan = models.CharField(max_length=120)
+    yettinchi_fan = models.CharField(max_length=120)
+    sakkizinchi_fan = models.CharField(max_length=120)
+
