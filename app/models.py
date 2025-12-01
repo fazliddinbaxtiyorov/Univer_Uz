76cae337947b5b6f8284c06fdab1e04b13a1feca
Fazliddin Baxtiyorov <arolboyevfazliddin956@gmail.com>
2025-12-01 16:07:19 +0500

    Editing Models

--- app/models.py
+++ app/models.py
@@ -4,19 +4,33 @@ from django.db import models
 
 
 class Fanlar(models.Model):
-    birinchi_fan = models.CharField(max_length=120)
-    ikkinchi_fan = models.CharField(max_length=120)
-    uchinchi_fan = models.CharField(max_length=120)
-    tortinchi_fan = models.CharField(max_length=120)
-    beshinchichi_fan = models.CharField(max_length=120)
+    Fan_CHOICES = (
+        ('Matematika', 'Matematika'),
+        ('Ona Tili', 'Ona Tili'),
+        ('Tarix', 'Tarix'),
+        ('Kimyo', 'Kimyo'),
+        ('Biologiya', 'Biologiya'),
+        ('Geografiya', 'Geografiya'),
+        ('Fizika', 'Fiziki'),
+        ('Ingliz Tili', 'Ingliz Tili'),
+    )
+    birinchi_fan = models.CharField(max_length=30, choices=Fan_CHOICES)
+    ikkinchi_fan = models.CharField(max_length=30, choices=Fan_CHOICES)
+    uchinchi_fan = models.CharField(max_length=30, default='Ona Tili')
+    tortinchi_fan = models.CharField(max_length=30, default='Matematika')
+    beshinchichi_fan = models.CharField(max_length=20, default='Tarix')
 
 
 class DTM_Practise(models.Model):
+    variant_choise = (
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D')
+    )
     savol = models.TextField()
-    variant = models.CharField(max_length=1)
-    togri_javob = models.CharField(max_length=1)
-    ball = models.IntegerField()
-    reyting = models.CharField(max_length=10)
+    togri_javob = models.CharField(max_length=1, choices=variant_choise)
+    ball = models.FloatField()
 
 
 class Students(models.Model):
